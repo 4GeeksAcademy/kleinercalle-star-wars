77e53582bd3ed4525b4e3872c5aee5054cbfcc16
kleinercalle <kleiner2024@gmail.com>
2024-09-11 22:17:04 +0000

    hice

--- src/front/js/Layout.jsx
+++ src/front/js/Layout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import injectContext from "./store/appContext.js";
+// custom component
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
-
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
+// custom pages 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
-import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+
+
 
 //create your first component
 const Layout = () => {
