9eede90c671d052ae427d138905a38a36bdec03a
kleinercalle <kleiner2024@gmail.com>
2024-10-25 22:47:16 +0000

    rest api star wars

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,12 +1,16 @@
 import os
 from flask_admin import Admin
+from api.models import db, Users, Character, Planet, Favorite  # Adjusted import path
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
-    admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
 
-    
+    # Add views for each model
+    admin.add_view(ModelView(Users, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
